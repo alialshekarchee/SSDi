e1bfb0d06abf39b1f0ad0b7c8c27c99d68a82d61
alialshekarchee <alielshekarchee@gmail.com>
2018-06-20 17:48:49 +0300

    Update SSDi.h

--- SSDi.h
+++ SSDi.h
@@ -1,3 +1,21 @@
+/*
+    SSDi.h - Library for driving 7-Segments display.
+    Created by Ali A. Alshekarchee, June 18, 2018.
+    Released into the public domain.
+    This file is part of SSDi library.
+    ////////////////////////////////////////////////////////////////////
+    SSDi library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    SSDi library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef SSDi_H
 #define SSDi_H
  
